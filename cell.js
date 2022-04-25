0dbe517491b8d23bce8f3c5ad897d6aca07c9f02
brigide <brigidegd@gmail.com>
2022-04-25 01:54:28 -0300

    grid config

--- cell.js
+++ cell.js
@@ -2,13 +2,19 @@ class Cell {
     constructor(i, j) {
         this.i = i;
         this.j = j;
+        this.walls = [true, true, true, true];
     }
 
     show() {
         let x = this.i * cellsize;
         let y = this.j * cellsize;
         stroke(255);
-        noFill();
-        rect(x, y, cellsize, cellsize);
+        if (this.walls[0]) line(x           , y           , x + cellsize, y);
+        if (this.walls[1]) line(x + cellsize, y           , x + cellsize, y + cellsize);
+        if (this.walls[2]) line(x + cellsize, y + cellsize, x           , y + cellsize);
+        if (this.walls[3]) line(x           , y + cellsize, x           , y);
+
+        // noFill();
+        // rect(x, y, cellsize, cellsize);
     }
 }
