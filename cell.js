dd5718c0e51c3405f83295ef233d650a5d6706d0
brigide <brigidegd@gmail.com>
2022-04-25 02:13:05 -0300

    checking random neighbors

--- cell.js
+++ cell.js
@@ -3,6 +3,7 @@ class Cell {
         this.i = i;
         this.j = j;
         this.walls = [true, true, true, true];
+        this.visited = false;
     }
 
     show() {
@@ -14,7 +15,36 @@ class Cell {
         if (this.walls[2]) line(x + cellsize, y + cellsize, x           , y + cellsize);
         if (this.walls[3]) line(x           , y + cellsize, x           , y);
 
-        // noFill();
-        // rect(x, y, cellsize, cellsize);
+        if (this.visited) {
+            fill(255, 0, 255, 100);
+            rect(x, y, cellsize, cellsize);
+        }
+    }
+
+    index(i, j) {
+        if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+            return -1;
+        }
+        return i + j * cols;
+    }
+
+    checkNeighbors() {
+        let neighbors = [];
+
+        let top = grid[this.index(this.i, this.j - 1)];
+        let right = grid[this.index(this.i + 1, this.j)];
+        let bottom = grid[this.index(this.i, this.j + 1)];
+        let left = grid[this.index(this.i - 1, this.j)];
+
+        if (top && !top.visited) neighbors.push(top);
+        if (right && !right.visited) neighbors.push(right);
+        if (bottom && !bottom.visited) neighbors.push(bottom);
+        if (left && !left.visited) neighbors.push(left);
+
+        if (neighbors.length > 0) {
+            let r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        }
+        return undefined;
     }
 }
--- sketch.js
+++ sketch.js
@@ -2,11 +2,15 @@ let cols, rows;
 let cellsize = 40;
 let grid = [];
 
+let current;
+
 function setup() {
     createCanvas(400, 400);
 
     cols = floor(width / cellsize);
     rows = floor(height / cellsize);
+
+    frameRate(5);
 
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols; i++) {
@@ -14,11 +18,20 @@ function setup() {
             grid.push(cell);
         }
     }
+
+    current = grid[0];
 }
 
 function draw() {
     background(51);
     for (let i in grid) {
         grid[i].show();
+    }
+
+    current.visited = true;
+    let next = current.checkNeighbors();
+    if (next) {
+        next.visited = true;
+        current = next;
     }
 }
